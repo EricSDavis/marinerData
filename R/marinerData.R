e230e659f50ebd0d8f3676aa029a5c142846d0d1
Eric Davis <ericscottdavis@outlook.com>
2023-03-30 17:24:33 -0400

    Create ExperimentHub package for mariner data.

---
+++ R/marinerData.R
@@ -0,0 +1,13 @@
+#' ExperimentHub datasets for the mariner package
+#'
+#' @description Subsampled Hi-C in HEK cells expressing
+#'  the NHA9 fusion with an F to S mutated IDR ("FS") or
+#'  without any mutations to the IDR ("Wildtype" or "WT").
+#'  These files are used for testing mariner functions and
+#'  some examples.
+#'
+#' @name marinerData
+#' @aliases marinerData marinerData
+#' @docType package
+#' @author Eric S. Davis
+NULL
---
+++ inst/scripts/make-metadata.R
@@ -0,0 +1,51 @@
+# A script to make the metadata.csv file located in inst/extdata of the package.
+# See ?AnnotationHubData::makeAnnotationHubMetadata for a description of the
+# metadata.csv file, expected fields and data types. This
+# AnnotationHubData::makeAnnotationHubMetadata() function can be used to
+# validate the metadata.csv file before submitting the package.
+
+metadata <- data.frame(
+    Title = c(
+        "LEUK_HEK_PJA27_inter_30.hic",
+        "LEUK_HEK_PJA30_inter_30.hic"
+    ),
+    Description = c(
+        paste(
+            "Subsampled Hi-C in HEK cells expressing the NHA9 fusion",
+            "with an F to S mutated IDR"
+        ),
+        paste(
+            "Subsampled Hi-C in HEK cells expressing the NHA9 fusion",
+            "with a Wildtype IDR"
+        )
+    ),
+    BiocVersion = rep("3.17", 2),
+    Genome = rep("hg19", 2),
+    SourceType = rep("TXT", 2),
+    SourceUrl = c(
+        paste0(
+            "https://zenodo.org/record/7786502/files/",
+            "LEUK_HEK_PJA27_inter_30.hic?download=1"
+        ),
+        paste0(
+            "https://zenodo.org/record/7786502/files/",
+            "LEUK_HEK_PJA30_inter_30.hic?download=1"
+        )
+    ),
+    SourceVersion = rep("v1", 2),
+    Species = rep("Homo sapiens", 2),
+    TaxonomyId = rep(9606, 2),
+    Coordinate_1_based = rep(FALSE, 2),
+    DataProvider = rep("Phanstiel Lab", 2),
+    Maintainer = rep("Eric S Davis <ericscottdavis@outlook.com>", 2),
+    RDataClass = rep(NA_character_, 2),
+    DispatchClass = rep("FilePath", 2),
+    Location_Prefix = rep("https://zenodo.org/record/7786502/files/", 2),
+    RDataPath = c(
+        "LEUK_HEK_PJA27_inter_30.hic?download=1",
+        "LEUK_HEK_PJA30_inter_30.hic?download=1"
+    ),
+    Tags = rep("Hi-C", 2)
+)
+
+write.csv(metadata, file="inst/extdata/metadata.csv", row.names=FALSE)
