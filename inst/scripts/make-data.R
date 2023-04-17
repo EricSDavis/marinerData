909152e1a445e009c356dd6dacbcff9a273b7dfb
Eric Davis <ericscottdavis@outlook.com>
2023-04-17 12:14:06 -0400

    Add information to make-data.R; Add useful tests;

--- inst/scripts/make-data.R
+++ inst/scripts/make-data.R
@@ -7,3 +7,22 @@
 # prior to hosting on the web site. For data hosted on a public web site with
 # no prior manipulation this file is not needed.
 NULL
+
+## Description of data sourcing
+
+## NHA9 .hic files and loop files #####
+# Subsampled Hi-C in HEK cells expressing
+# the NHA9 fusion with an F to S mutated IDR ("FS") or
+# without any mutations to the IDR ("Wildtype" or "WT").
+# Accompanying loop calls identified using SIP
+# (https://github.com/PouletAxel/SIP) are also provided.
+# For more information or full datasets, see
+# https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSE143465.
+
+## LPS Induced Macrophage Activation loop files #####
+# Also included are loop calls identified using SIP
+# (https://github.com/PouletAxel/SIP) from a THP-1
+# activation timecourse after stimulation with LPS & IF-G
+# for 0, 30, 60, 80, 120, 2440, 360, or 1440 minutes.
+# For more information or full datasets, see
+# https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSE201353.
--- tests/testthat/test_metadata.R
+++ tests/testthat/test_metadata.R
@@ -1,10 +1,22 @@
-test_that("metadata is valid",
-{
-    ## Won't pass devtools::check() because
-    ## makeExperimentHubMetadata() looks for "inst/extdata/metadata"
-    ## but installed packages use path "extdata/metadata"
-    # path <- find.package("marinerData")
-    # md <- system.file("extdata", "metadata.csv", package="marinerData")
-    # expect_error(ExperimentHubData::makeExperimentHubMetadata(path), NA)
-    expect_true(TRUE)
+test_that("Accessors work", {
+
+    ## Test that files are accessible
+    doTest <- \(x) expect_true(file.exists(x))
+
+    suppressMessages({
+        doTest(marinerData::FS_5kbLoops.txt())
+        doTest(marinerData::FS_5kbLoops.txt())
+        doTest(marinerData::LEUK_HEK_PJA27_inter_30.hic())
+        doTest(marinerData::LEUK_HEK_PJA30_inter_30.hic())
+        doTest(marinerData::LIMA_0000.bedpe())
+        doTest(marinerData::LIMA_0030.bedpe())
+        doTest(marinerData::LIMA_0060.bedpe())
+        doTest(marinerData::LIMA_0090.bedpe())
+        doTest(marinerData::LIMA_0120.bedpe())
+        doTest(marinerData::LIMA_0240.bedpe())
+        doTest(marinerData::LIMA_0360.bedpe())
+        doTest(marinerData::LIMA_1440.bedpe())
+        doTest(marinerData::WT_5kbLoops.txt())
+    })
+
 })
